f4c2a8b3c08181cc88982f63ccaa8eec002a86f3
Zak Cole <zakcole08@gmail.com>
2024-02-14 12:30:24 +0000

    amended formatting of displayed message

--- Sort-FilesByID(parallel).ps1
+++ Sort-FilesByID(parallel).ps1
@@ -1,17 +1,19 @@
 Clear-Host
-Write-Host " _____            _    ______ _ _            ______         ___________ 
+Write-Host -foregroundcolor Blue " _____            _    ______ _ _            ______         ___________ 
 /  ___|          | |   |  ___(_) |           | ___ \       |_   _|  _  \
-\ `--.  ___  _ __| |_  | |_   _| | ___  ___  | |_/ /_   _    | | | | | |
- `--. \/ _ \| '__| __| |  _| | | |/ _ \/ __| | ___ \ | | |   | | | | | |
+\ `--.   ___  _ __| |_  | |_   _| | ___  ___  | |_/ /_   _    | | | | | |
+ `--. \ / _ \| '__| __| |  _| | | |/ _ \/ __| | ___ \ | | |   | | | | | |
 /\__/ / (_) | |  | |_  | |   | | |  __/\__ \ | |_/ / |_| |  _| |_| |/ / 
 \____/ \___/|_|   \__| \_|   |_|_|\___||___/ \____/ \__, |  \___/|___/  
                                                      __/ |              
                                                     |___/               
-This script will move files from the 'Source Folder' containing an ID (first number in the filename) to the Directory/Sub-directory with the corresponding ID in the 'Destination Folder'.
+"
+Write-Host "This script will move files from the 'Source Folder' containing an ID (first number in the filename) to the Directory/Sub-directory with the corresponding ID in the 'Destination Folder'.
 ----------------------------------------------------------------------------
 Source Folder: The folder containing the documents you would like to move
 Destination Folder: The folder on the N:\ drive to move them to (HRBP)
 Filename: [Folder]\[Filename.docx]"
+Write-Host ""
 
 $sourcefolder = Read-Host -Prompt 'Enter Source Folder'
 $destfolder = Read-Host -Prompt 'Enter Destination Folder'
