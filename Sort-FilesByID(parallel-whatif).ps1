4d23b345cfa62a86a360db260a862499086589bf
Zak Cole <zakcole08@gmail.com>
2024-02-14 12:38:12 +0000

    amended formatting of displayed message

--- Sort-FilesByID(parallel-whatif).ps1
+++ Sort-FilesByID(parallel-whatif).ps1
@@ -11,7 +11,7 @@ Write-Host -foregroundcolor Blue " _____            _    ______ _ _            _
 Write-Host "This script will move files from the 'Source Folder' containing an ID (first number in the filename) to the Directory/Sub-directory with the corresponding ID in the 'Destination Folder'.
 ----------------------------------------------------------------------------
 Source Folder: The folder containing the documents you would like to move
-Destination Folder: The folder on the N:\ drive to move them to (HRBP)
+Destination Folder: The target location to move the files into. (Should contain directories/sub-directories with ID numbers)
 Filename: [Folder]\[Filename.docx]"
 Write-Host ""
 
